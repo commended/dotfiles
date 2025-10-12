9aa50a7814c6173809b43f848382492daec57773
aug <squadwipe@protonmail.com>
2025-10-11 20:57:59 -0700

    Update and rename dashboard.lua to veil.lua

---
+++ nvim/plugins/veil.lua
@@ -0,0 +1,12 @@
+return {
+  "willothy/veil.nvim",
+  lazy = false,
+  dependencies = {
+    -- All optional, only required for the default setup.
+    -- If you customize your config, these aren't necessary.
+    "nvim-telescope/telescope.nvim",
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope-file-browser.nvim",
+  },
+  config = true,
+}
