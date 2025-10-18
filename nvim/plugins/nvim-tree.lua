18862ce430b73b4f4ba3bb072dcb7496b71361d5
aug <squadwipe@protonmail.com>
2025-10-18 11:06:02 -0700

    Add nvim-tree.lua configuration for Neovim

---
+++ nvim/plugins/nvim-tree.lua
@@ -0,0 +1,40 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    enabled = false,
+  },
+
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      vim.g.loaded_netrw = 1
+      vim.g.loaded_netrwPlugin = 1
+
+      require("nvim-tree").setup({
+        sort_by = "case_sensitive",
+        view = {
+          width = 30,
+        },
+        renderer = {
+          group_empty = true,
+          icons = {
+            show = {
+              file = true,
+              folder = true,
+              folder_arrow = true,
+              git = true,
+            },
+          },
+        },
+        filters = {
+          dotfiles = false,
+        },
+      })
+    end,
+  },
+}
