1993ab5ae38a6116b1192c9e96284c58783d9f58
aug <squadwipe@protonmail.com>
2025-10-18 06:50:54 -0700

    Update colorscheme configuration for transparency
    Set background color to none for Normal, NormalFloat, and NormalNC highlights.

--- nvim/plugins/colorscheme.lua
+++ nvim/plugins/colorscheme.lua
@@ -3,6 +3,10 @@ return {
     "kdheepak/monochrome.nvim",
     config = function()
       vim.cmd("colorscheme monochrome")
+
+      vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+      vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
     end,
   },
 }
